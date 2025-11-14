2a07e328a481bc2b589338e022d6c10e4c3a38bd
Benibox <ethanbenamram99@gmail.com>
2025-11-14 10:21:29 +0200

    Fix build by removing remote fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -329,7 +329,7 @@ export default function Page() {
       return;
     }
 
-    let frame = requestAnimationFrame(() => setShowHeroLogo(true));
+    const frame = requestAnimationFrame(() => setShowHeroLogo(true));
     return () => {
       cancelAnimationFrame(frame);
       setShowHeroLogo(false);

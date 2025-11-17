227971ab764b59aaf54aa5bc637b03d36cff7254
Benibox <ethanbenamram99@gmail.com>
2025-11-17 19:17:25 +0100

    Fix robots metadata keys

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export const metadata: Metadata = {
     googleBot: {
       index: true,
       follow: true,
-      maxImagePreview: "large",
-      maxSnippet: -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
     },
   },
   alternates: {
